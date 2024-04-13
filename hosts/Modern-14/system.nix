083370eb1e2135a8b4594ef16ce3e8fc0190208d
dretyuiop <chewch03@gmail.com>
2024-04-13 23:51:25 +0800

    correct hostname

--- hosts/Modern-14/system.nix
+++ hosts/Modern-14/system.nix
@@ -3,13 +3,8 @@
 {
   imports =
     [
-      # Nvidia
-      ../../modules/hardware/nvidia
-
-      # Gaming
-      ../../modules/gaming/system.nix
     ];
 
     # Hostname
-    networking.hostName = "Dell-G15-5525";
+    networking.hostName = "Modern-14";
 }
