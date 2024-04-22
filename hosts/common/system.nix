b7b4f11d7f44877bef08478baf1728490a9cc82b
dretyuiop <chewch03@gmail.com>
2024-04-22 17:41:10 +0800

    appimage support

--- hosts/common/system.nix
+++ hosts/common/system.nix
@@ -221,6 +221,8 @@
         attr
         bzip2
         curl
+        fuse
+        icu
         kdePackages.full
         libsodium
         libssh
@@ -237,6 +239,15 @@
     };
   };
 
+  # appimage
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
